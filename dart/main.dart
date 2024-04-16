b1adc1b1abe2170cb6592cc9ccccd76ffcfa15f8
MuhammadShairaz <162301732+MuhammadShairaz@users.noreply.github.com>
2024-04-16 09:25:20 -0700

    Add files via upload

---
+++ dart/main.dart
@@ -0,0 +1,14 @@
+void main(){
+  print('Helow World');
+  int num =70;
+  int num_2=40;
+  double double_num =10.5;
+  bool bool_val =true;
+  var var_name='learning Dart';
+
+  print(num + num_2);
+  print(num - num_2);
+  print(num * num_2);
+   print(num / num_2);
+   print(num % num_2);
+}
